d0cb357bb6b754cd2b9f1f7e8035e93b3dac65b9
Gabriel Valenca <72149653+GabValenca@users.noreply.github.com>
2024-03-08 18:12:57 -0300

    Alterado o https

--- build/server.js
+++ build/server.js
@@ -12,29 +12,29 @@ app.get('/', (req, res) => {
 });
 
 // Certificado privado e público
-const privateKey = fs.readFileSync('/etc/ssl/blackparrot.com.br/blackparrot.com.br.key', 'utf8');
-const certificate = fs.readFileSync('/etc/ssl/blackparrot.com.br/blackparrot.com.br.crt', 'utf8');
+//const privateKey = fs.readFileSync('/etc/ssl/blackparrot.com.br/blackparrot.com.br.key', 'utf8');
+//const certificate = fs.readFileSync('/etc/ssl/blackparrot.com.br/blackparrot.com.br.crt', 'utf8');
 
-const credentials = { key: privateKey, cert: certificate };
+//const credentials = { key: privateKey, cert: certificate };
 
 // Inicia o servidor HTTP para redirecionar automaticamente para HTTPS
 const httpServer = http.createServer((req, res) => {
-  res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url });
+  res.writeHead(301, { 'Location': 'http://' + req.headers.host + req.url });
   res.end();
 });
 
 // Inicia o servidor HTTP na porta 80
-httpServer.listen(80, () => {
-  console.log('Servidor HTTP está rodando na porta 80 (redirecionando para HTTPS)');
-});
+//httpServer.listen(80, () => {
+//  console.log('Servidor HTTP está rodando na porta 80 (redirecionando para HTTPS)');
+//});
 
 // Configuração para servir arquivos estáticos
-app.use(express.static('/build/'));
+app.use(express.static('/app-repo/Black-Parrot-teste/build/'));
 
 // Cria o servidor HTTPS
-const httpsServer = https.createServer(credentials, app);
+//const httpsServer = https.createServer(credentials, app);
 
 // Inicia o servidor HTTPS na porta 443
-httpsServer.listen(443, () => {
-  console.log('Servidor HTTPS está rodando na porta 443');
-});
+//httpsServer.listen(443, () => {
+//  console.log('Servidor HTTPS está rodando na porta 443');
+//});
